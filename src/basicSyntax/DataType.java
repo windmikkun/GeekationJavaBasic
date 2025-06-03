5f7de859f9a482cf9a1490973f9a6fe7f90fafa9
windmikkun <windmikkun@gmail.com>
2025-06-03 20:36:59 +0900

    Datatypeの実装

---
+++ src/basicSyntax/DataType.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str1 = "こんにちは";
+		String str2 = "私は";
+		String str3 = "Geek君です。";
+		
+		System.out.println( str1 + str2 + str3 );
+		System.out.println( 10 );
+		System.out.println( "20" + "24" );
+		System.out.println(3.14);
+		System.out.println(true);
+	}
+
+}
