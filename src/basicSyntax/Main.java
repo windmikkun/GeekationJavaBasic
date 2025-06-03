fc5cf80eaed225e21be1dff9d5fe5755a7564634
windmikkun <windmikkun@gmail.com>
2025-06-03 20:21:54 +0900

    added Main.java String practice

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+	System.out.println("こんにちは");
+	System.out.println("私の名前は");
+	System.out.println("Geek君です。");
+	
+	//文字列は+で連結することができます
+	System.out.println("こんにちは" +"私の名前は" + "Geek君です。");
+	}
+
+}
