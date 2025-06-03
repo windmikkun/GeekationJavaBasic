91222a6ee59f4eee55e0c958abe70fcae5302c88
windmikkun <windmikkun@gmail.com>
2025-06-04 03:43:51 +0900

    Variablesの実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,21 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 3910;
+		double num2 = 1369.92626;
+		
+		String type = "整数";
+		String base = "私の好きな" + type + "は";
+		String end = "です。";
+		System.out.println(base + num1 + end);
+		
+		type = "小数点";
+		base = "私の好きな" + type + "は";
+		System.out.println(base + num2 + end);
+		
+	}
+
+}
